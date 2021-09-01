994f47ea7f69aebaf2dcad3654b018a0988dac7d
merveeatay <83241309+merveeatay@users.noreply.github.com>
2021-09-01 19:36:25 +0300

    Palindrom Kelime Sayı

---
+++ PalindromikKelimeSayı/PalindromikKelimeSayı/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace PalindromikKelimeSayı
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Girilen bir sayı ya da kelimenin palindromik olup olmadığını sorgulama:
+
+            Console.WriteLine("  BİR SAYI YA DA YAZI GİRİŞİ YAPINIZ :  ");
+            string kelime = Console.ReadLine(  );
+            string kelimeTers = "";
+
+            for (int i = kelime.Length-1; i >= 0; i--)
+            {
+                kelimeTers += kelime[i];
+            }
+            Console.WriteLine("  Girdiğiniz sayı ya da yazının tersi===>" + " " + kelimeTers  );
+            if (kelime == kelimeTers)
+            {
+                Console.WriteLine("  Palindromiktir.  ");
+            }
+            else
+            {
+                Console.WriteLine("  Palindromik değildir.");
+            }
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
